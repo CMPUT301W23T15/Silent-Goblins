16f497a6133c45356e7d829904c90a72c7ebec74
Zachary N <zwnathoo@ualberta.ca>
2023-01-31 19:33:16 -0700

    added x+y

--- app/src/main/java/com/example/silent_goblins/Octagon.java
+++ app/src/main/java/com/example/silent_goblins/Octagon.java
@@ -3,6 +3,6 @@ package com.example.silent_goblins;
 public class Octagon extends Shape{
     public Octagon (int x, int y) {
         this.x = x;
-        this.y = x;
+        this.y = x + y;
     }
 }
